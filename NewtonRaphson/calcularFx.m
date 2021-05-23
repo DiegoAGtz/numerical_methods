823c3ad6cad4ee857a1b22d06c8f1dde066c369d
DiegoAG <diegogtz3435@gmail.com>
2021-05-22 22:48:14 -0500

    Se añadio el método de NR y NR de segundo orden

---
+++ NewtonRaphson/calcularFx.m
@@ -0,0 +1,31 @@
+## Copyright (C) 2021 Diego Armando Gutiérrez Ayala
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {@var{retval} =} calcularFx (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Diego Armando Gutiérrez Ayala <diego@ag>
+## Created: 2021-02-04
+
+function fx = calcularFx (x)
+   #fx = x**2 - e**x - 5*x;
+   fx = x - 4.28*e^(-1.4*x) - 181.49;
+   #fx = cos(4*x);
+   #fx = (4*x - 7)/(x - 2);
+   #fx = x^3 + 2.*x^2 + 10.*x - 20;  
+endfunction
